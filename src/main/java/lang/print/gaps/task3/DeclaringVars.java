97e3e360a47b3537e65bbdecbd4411645c52f609
komronz <pandacoder0820@gmail.com>
2022-07-28 16:32:09 +0500

    module3Task3

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -3,9 +3,7 @@ package lang.print.gaps.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
         int a, b,c;
-        a = 10;
-        b = 10;
-        c = 10;
+        a=b=c=10;
         System.out.println(c);
     }
 }
